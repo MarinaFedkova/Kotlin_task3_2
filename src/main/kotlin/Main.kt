e3faa7350f4596da19a2118f798355325141a0fd
Marina Fedkova <mzaloznaya@ya.ru>
2021-01-20 09:57:48 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,53 @@
+fun main() {
+    countComission("Vk Pay", 410_000, 5_000, 2_000)
+}
+
+fun countComission(
+    cardType: String = "Vk Pay",
+    totalAmountOfMonth: Int = 0,
+    totalAmountOfDay: Int = 0,
+    amount: Int
+) {
+    when (cardType) {
+        "Mastercard", "Maestro" -> countComissionMastercardNaestro(amount, totalAmountOfMonth, totalAmountOfDay)
+        "Visa", "Мир" -> countComissionVisaMir(amount, totalAmountOfMonth, totalAmountOfDay)
+        "Vk Pay" -> countComissionVkPay(totalAmountOfMonth, totalAmountOfDay)
+    }
+}
+
+fun countComissionMastercardNaestro(
+    amount: Int,
+    totalAmountOfMonth: Int = 0,
+    totalAmountOfDay: Int = 0,
+) {
+    when {
+        totalAmountOfDay > 150_000 -> println("В операции отказано. Превышен лимит суммы за день")
+        totalAmountOfMonth > 600_000 -> println("В операции отказано. Превышен лимит суммы за месяц")
+        totalAmountOfMonth > 75_000 -> println("Комиссия за перевод составит ${amount * 0.006 + 20.00} руб.")
+        else -> println("Комиссия за перевод отсутствует")
+    }
+}
+
+fun countComissionVisaMir(
+    amount: Int,
+    totalAmountOfMonth: Int = 0,
+    totalAmountOfDay: Int = 0,
+) {
+    when {
+        totalAmountOfDay > 150_000 -> println("В операции отказано. Превышен лимит суммы за день")
+        totalAmountOfMonth > 600_000 -> println("В операции отказано. Превышен лимит суммы за месяц")
+        amount * 0.0075 < 35 -> println("Комиссия за перевод составит 35 руб.")
+        else -> println("Комиссия за перевод составит ${amount * 0.0075} руб.")
+    }
+}
+
+fun countComissionVkPay(
+    totalAmountOfMonth: Int = 0,
+    totalAmountOfDay: Int = 0,
+) {
+    when {
+        totalAmountOfDay > 15_000 -> println("В операции отказано. Превышен лимит суммы за день")
+        totalAmountOfMonth > 40_000 -> println("В операции отказано. Превышен лимит суммы за месяц")
+        else -> println("Комиссия за перевод отсутствует")
+    }
+}
